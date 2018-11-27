2e9a9d3f6177d602c7bf1af5cb3acd53f8ee0cc6
Mieczan <mieczandawid@gmail.com>
2018-11-27 00:26:35 +0000

    Register user and validate

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,66 @@ router.get('/login', function(req, res){
 	res.render('login'); 
 });
 
+//Register user
+router.post('/register', function(req, res){ 
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+
+	console.log(name);
+
+	// Validation
+	req.checkBody('name', 'Name is required').notEmpty();
+	req.checkBody('email', 'Email is required').notEmpty();
+	req.checkBody('email', 'Email is not valid').isEmail();
+	req.checkBody('username', 'Username is required').notEmpty();
+	req.checkBody('password', 'Password is required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+	var errors = req.validationErrors();
+
+
+	if (errors) {
+		//re-render the form 
+		res.render('register', {
+			errors: errors
+		});
+	}
+	else {
+		//checking for email and username are already taken
+		User.findOne({ username: { 
+			"$regex": "^" + username + "\\b", "$options": "i"
+	}}, function (err, user) {
+			User.findOne({ email: { 
+				"$regex": "^" + email + "\\b", "$options": "i"
+		}}, function (err, mail) {
+				if (user || mail) {
+					res.render('register', {
+						user: user,
+						mail: mail
+					});
+				}
+				else {
+					var newUser = new User({
+						name: name,
+						email: email,
+						username: username,
+						password: password
+					});
+					User.createUser(newUser, function (err, user) {
+						if (err) throw err;
+						console.log(user);
+					});
+         	req.flash('success_msg', 'You are registered and can now login');
+					res.redirect('/users/login');
+				}
+			});
+		});
+	}
+
+	
+});
+
 module.exports = router;
